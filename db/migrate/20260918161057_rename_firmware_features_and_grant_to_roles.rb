2d39a90bfcf48d19ccad1dce963ee44e86e954c3
Gilbert Cherrie <g.cherrie@outlook.com>
2026-09-23 12:00:52 -0400

    Fix rubocop issues

--- db/migrate/20260918161057_rename_firmware_features_and_grant_to_roles.rb
+++ db/migrate/20260918161057_rename_firmware_features_and_grant_to_roles.rb
@@ -19,13 +19,13 @@ class RenameFirmwareFeaturesAndGrantToRoles < ActiveRecord::Migration[8.0]
   def up
     return if MiqProductFeature.none?
 
-    say_with_time 'Renaming generic firmware features to firmware_registry' do
+    say_with_time('Renaming generic firmware features to firmware_registry') do
       FEATURE_MAPPING.each do |from, to|
         MiqProductFeature.find_by(:identifier => from)&.update!(:identifier => to)
       end
     end
 
-    say_with_time 'Granting firmware_binary and firmware_target features to roles with firmware_registry features' do
+    say_with_time('Granting firmware_binary and firmware_target features to roles with firmware_registry features') do
       NEW_FEATURE_SETS.each do |source_ident, target_idents|
         source_feature = MiqProductFeature.find_by(:identifier => source_ident)
         next unless source_feature
@@ -47,14 +47,14 @@ class RenameFirmwareFeaturesAndGrantToRoles < ActiveRecord::Migration[8.0]
   def down
     return if MiqProductFeature.none?
 
-    say_with_time 'Removing firmware_binary and firmware_target role mappings and features' do
+    say_with_time('Removing firmware_binary and firmware_target role mappings and features') do
       new_identifiers = NEW_FEATURE_SETS.values.flatten.uniq
       new_features = MiqProductFeature.where(:identifier => new_identifiers)
       MiqRolesFeature.where(:miq_product_feature_id => new_features.select(:id)).delete_all
       new_features.delete_all
     end
 
-    say_with_time 'Reverting firmware_registry features back to generic firmware' do
+    say_with_time('Reverting firmware_registry features back to generic firmware') do
       FEATURE_MAPPING.each do |from, to|
         MiqProductFeature.find_by(:identifier => to)&.update!(:identifier => from)
       end
